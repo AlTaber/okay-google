bb8ec6dc3b1365f7445c2ebafcae86ddcc98e3f9
saks <prohapro@gmail.com>
2022-02-03 17:53:54 +0200

    задание №1 готово(рил)

--- main.py
+++ main.py
@@ -10,16 +10,23 @@ class Searcher(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.initUi()
+        self.image = QLabel(self)
+        self.reloadMap()
 
-    def initUi(self):
-        data = requests.get('https://static-maps.yandex.ru/1.x/?ll=19.9026511,54.6432638&spn=0.1,0.1&l=sat').content
+    def reloadMap(self, coords='19.9026511,54.6432638'):  # 19.9026511,54.6432638
+        self.image.clear()
+        data = requests.get(f'https://static-maps.yandex.ru/1.x/?ll={coords}&spn=0.1,0.1&l=sat').content
         self.pixmap = QPixmap()
         self.pixmap.loadFromData(data)
-        self.image = QLabel(self)
         self.image.move(5, 100)
         self.image.resize(500, 350)
         self.image.setPixmap(self.pixmap)
+        self.pushButton.clicked.connect(self.set_img)
+
+    def set_img(self):
+        print(self.lineEdit.text())
+        self.reloadMap(self.lineEdit.text())
+
 
 
 if __name__ == '__main__':
