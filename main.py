3a7a3c1439a3b8206d106d7152b854af9098e723
andrd <andr.dorokhov123@gmail.com>
2022-02-03 20:00:08 +0200

    добавил поисковую строку

--- main.py
+++ main.py
@@ -12,27 +12,24 @@ class Searcher(QMainWindow, Ui_MainWindow):
         self.setupUi(self)
         self.image = QLabel(self)
         self.coords = '19.9026511,54.6432638'
+        self.obj = ''
         self.map_format = "sat"
         self.reloadMap()
 
         self.radioButton.toggled.connect(self.change_map_format)
         self.radioButton_2.toggled.connect(self.change_map_format)
         self.radioButton_3.toggled.connect(self.change_map_format)
+        self.pushButton.clicked.connect(self.search)
 
     def reloadMap(self):  # 19.9026511,54.6432638
         self.image.clear()
-        data = requests.get(f'https://static-maps.yandex.ru/1.x/?ll={self.coords}&spn=0.1,0.1&l={self.map_format}').content
+        data = requests.get(
+            f'https://static-maps.yandex.ru/1.x/?ll={self.coords}&spn=0.1,0.1&l={self.map_format}').content
         self.pixmap = QPixmap()
         self.pixmap.loadFromData(data)
         self.image.move(5, 100)
         self.image.resize(500, 350)
         self.image.setPixmap(self.pixmap)
-        self.pushButton.clicked.connect(self.set_img)
-
-    def set_img(self):
-        print(self.lineEdit.text())
-        self.coords = self.lineEdit.text()
-        self.reloadMap()
 
     def change_map_format(self):
         if self.sender().text() == "Спутник":
@@ -42,6 +39,28 @@ class Searcher(QMainWindow, Ui_MainWindow):
         elif self.sender().text() == "Гибрид":
             self.map_format = "sat,skl"
         self.reloadMap()
+
+    def search(self):
+        self.obj = self.lineEdit.text()
+        toponym_to_find = self.obj
+        geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+
+        geocoder_params = {
+            "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+            "geocode": toponym_to_find,
+            "format": "json"}
+        response = requests.get(geocoder_api_server, params=geocoder_params)
+        json_response = response.json()
+        toponym = json_response["response"]["GeoObjectCollection"][
+            "featureMember"][0]["GeoObject"]
+        toponym_coodrinates = toponym["Point"]["pos"]
+        toponym_a, toponym_b = toponym_coodrinates.split(" ")
+        self.coords = f'{toponym_a},{toponym_b}'
+        print(toponym_coodrinates)
+        try:
+            self.reloadMap()
+        except:
+            print('error')
 
 
 if __name__ == '__main__':
